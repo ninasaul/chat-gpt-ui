d31160e2ca28b3b80cfb440c6691478ac5be60ea
ninasaul <ninasaul@protonmail.com>
2023-05-05 18:30:39 +0800

    feat(ui): build base ui component

--- src/chat/component/EmptyChat.jsx
+++ src/chat/component/EmptyChat.jsx
@@ -12,7 +12,7 @@ export function EmptyChat() {
       <EmptyImg />
       <div className={styles.empty_inner}>
         <Input onChange={(val) => setApiKey(val)} placeholder="OpenAI API key" className={styles.empty_input} />
-        <Button onClick={setModel({ apiKey })}>Save</Button>
+        <Button onClick={() => setModel({ apiKey })}>Save</Button>
       </div>
       <div className={styles.empty_text}>API key is stored locally. Create one on OpenAI</div>
     </div>
