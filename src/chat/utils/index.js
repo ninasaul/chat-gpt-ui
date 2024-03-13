f00896eede8b27f1863ea9d25ef7e9237c89ea24
Christian Gawron <gawron.christian@fh-swf.de>
2024-03-13 14:06:37 +0000

    fix: sha256 hash for gravatar

--- src/chat/utils/index.js
+++ src/chat/utils/index.js
@@ -28,8 +28,11 @@ export function dateFormat(ms) {
 }
 
 export async function sha256Digest(message) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(message);
-  const hash = await crypto.subtle.digest("SHA-256", data);
-  return hash;
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join(""); // convert bytes to hex string
+  return hashHex;
 }
