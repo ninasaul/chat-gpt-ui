18d3dbc7e313d346175787c63126a511f1df6b53
Christian Gawron <gawron.christian@fh-swf.de>
2024-03-15 15:51:26 +0000

    Add tooltips on sidebar

--- src/chat/ChatSideBar.jsx
+++ src/chat/ChatSideBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { Avatar, Icon } from '../components'
+import { Avatar, Icon, Tooltip } from '../components'
 import { useGlobal } from './context'
 import styles from './style/sider.module.less'
 import { classnames } from '../components/utils'
 import { useOptions } from './hooks'
+
+const Option = (props) => {
+  const { type, onClick, tooltip } = props
+  return (
+    <Tooltip text={tooltip}>
+      <Icon className={styles.icon} type={type} onClick={onClick} />
+    </Tooltip>
+  )
+}
 
 export function ChatSideBar() {
   const { is, setState, options, user } = useGlobal()
@@ -12,11 +21,12 @@ export function ChatSideBar() {
     <div className={classnames(styles.sider, 'flex-c-sb flex-column')}>
       <Avatar src={user?.avatar || ""} />
       <div className={classnames(styles.tool, 'flex-c-sb flex-column')}>
-        <Icon className={styles.icon} type="apps" onClick={() => setState({ is: { ...is, apps: true } })} />
-        <Icon className={styles.icon} type="history" onClick={() => setState({ is: { ...is, apps: false } })} />
-        <Icon className={styles.icon} type={options.general.theme} onClick={() => setGeneral({ theme: options.general.theme === 'light' ? 'dark' : 'light' })} />
-        <Icon className={styles.icon} type="config" onClick={() => setState({ is: { ...is, config: !is.config } })} />
-        <Icon className={styles.icon} type={`${is.fullScreen ? 'min' : 'full'}-screen`} onClick={() => setState({ is: { ...is, fullScreen: !is.fullScreen } })} />
+        <Option type="apps" onClick={() => setState({ is: { ...is, apps: true } })} tooltip="Apps" />
+        <Option type="history" onClick={() => setState({ is: { ...is, apps: false } })} tooltip="History" />
+        <Option type={options.general.theme} onClick={() => setGeneral({ theme: options.general.theme === 'light' ? 'dark' : 'light' })} tooltip="Theme" />
+        <Option type="config" onClick={() => setState({ is: { ...is, config: !is.config } })} tooltip="Config" />
+        <Option type={`${is.fullScreen ? 'min' : 'full'}-screen`} onClick={() => setState({ is: { ...is, fullScreen: !is.fullScreen } })}
+          tooltip={`${is.fullScreen ? 'Minimize' : 'Maximize'}`} />
       </div>
     </div>
   )
