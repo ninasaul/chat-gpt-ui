ea31eb92caf4144e6c4a146b6b80b18a83963393
Christian Gawron <gawron.christian@fh-swf.de>
2024-03-15 13:56:42 +0000

    data cleanup

--- src/chat/context/action.js
+++ src/chat/context/action.js
@@ -149,6 +149,7 @@ export default function action(state, dispatch) {
     },
 
     removeMessage(index) {
+      console.log("removeMessage", index);
       const messages = state.chat[state.currentChat].messages;
       const chat = [...state.chat];
       messages.splice(index, 1);
@@ -187,25 +188,3 @@ export default function action(state, dispatch) {
   };
 }
 
-export const datas = {
-  id: "chatcmpl-7AEK9Dlw96m5TejBKIKUgjyUHVTCa",
-  object: "chat.completion",
-  created: 1682672697,
-  model: "gpt-3.5-turbo-0301",
-  usage: {
-    prompt_tokens: 34,
-    completion_tokens: 303,
-    total_tokens: 337,
-  },
-  choices: [
-    {
-      message: {
-        role: "assistant",
-        content:
-          '好的，以下是一个简单的useKeyboard hooks。\n\n```jsx\nimport { useState, useEffect } from "react"; \n\nexport default function useKeyboard(targetKey) { \n  const [keyPressed, setKeyPressed] = useState(false); \n  \n  const downHandler = ({ key }) => {\n    if (key === targetKey) {\n      setKeyPressed(true); \n    } \n  }; \n  \n  const upHandler = ({ key }) => { \n    if (key === targetKey) { \n      setKeyPressed(false); \n    } \n  }; \n\n  useEffect(() => { \n    window.addEventListener("keydown", downHandler); \n    window.addEventListener("keyup", upHandler); \n    \n    return () => { \n      window.removeEventListener("keydown", downHandler); \n      window.removeEventListener("keyup", upHandler); \n    }; \n  }, []); \n\n  return keyPressed; \n}\n```\n\n这个hook将传递给它的按键(targetKey)与键盘按下事件进行比较。如果按键与传递进来的按键相同，那么hook的返回值(keyPressed)将被设置为true。否则，返回值为false。这个hook使用了React的useState和useEffect钩子函数。在useEffect中，我们添加按键按下和松开事件的监听器。当组件卸载时，我们移除这些监听器。',
-      },
-      finish_reason: "stop",
-      index: 0,
-    },
-  ],
-};
